a0c4004e70c631579bc3e3bd2d66060bb2b15b1a
Amine Baba <ababa@rbbn.com>
2021-12-04 13:31:36 -0500

    initial commit

--- src/main/java/com/cardex/microservices/cardexpropertyaccessservice/controller/PropertyFileAccessController.java
+++ src/main/java/com/cardex/microservices/cardexpropertyaccessservice/controller/PropertyFileAccessController.java
@@ -3,9 +3,15 @@ package com.cardex.microservices.cardexpropertyaccessservice.controller;
 import com.cardex.microservices.cardexpropertyaccessservice.beans.PropertyAccessBean;
 import com.cardex.microservices.cardexpropertyaccessservice.beans.PropertyAccessValue;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpEntity;
 
 @RestController
 @RequestMapping("/access")
@@ -14,15 +20,25 @@ public class PropertyFileAccessController {
     @Autowired
     PropertyAccessBean propertyAccessBean;
 
-    public enum RequestMethod {
-        GET, HEAD, POST, PUT, PATCH, DELETE, OPTIONS, TRACE
-    }
     @GetMapping("accessPropertyFile")
     public PropertyAccessValue accessPropertyFile(){
 
+        refreshActuator();
         return new PropertyAccessValue(propertyAccessBean.getName(),
                 propertyAccessBean.getDescription());
     }
 
+    public void refreshActuator(){
+        RestTemplate restTemplate = new RestTemplate();
+        final String baseUrl="http://localhost:8100/actuator/refresh ";
 
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Content-Type", "application/json");
+        HttpEntity entity = new HttpEntity(headers);
+
+        ResponseEntity<String> results=restTemplate.postForEntity(baseUrl,entity,String.class);
+
+
+
+    }
 }
