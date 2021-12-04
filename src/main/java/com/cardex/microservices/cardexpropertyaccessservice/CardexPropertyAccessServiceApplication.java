d02b0601d558f83ee6f12d5e357cf0678eeb276b
Amine Baba <ababa@rbbn.com>
2021-12-04 04:45:27 -0500

    initial commit

---
+++ src/main/java/com/cardex/microservices/cardexpropertyaccessservice/CardexPropertyAccessServiceApplication.java
@@ -0,0 +1,13 @@
+package com.cardex.microservices.cardexpropertyaccessservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardexPropertyAccessServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CardexPropertyAccessServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cardex/microservices/cardexpropertyaccessservice/CardexPropertyAccessServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cardex.microservices.cardexpropertyaccessservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardexPropertyAccessServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
